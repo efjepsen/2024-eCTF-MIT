39d944a08bfe1fa2ee8a51088bc396e2e9936a2f
Fisher Jepsen <efjepsen@mit.edu>
2024-03-09 19:38:31 -0500

    UNSAFE: Use \x00*64 as shared secret
    This is bad and unsafe. But it prevents us from having to rebuild
    all components and flash all components in between builds. Please
    remember to delete this commit before handoff. Please.

--- deployment/gen_global_secrets.py
+++ deployment/gen_global_secrets.py
@@ -7,6 +7,12 @@ OUTPUT_FILE = "global_secrets.h"
 with open("/dev/random", "rb") as f:
     secret = f.read(KEY_SIZE).hex()
 
+# TODO WARNING CRITICAL THIS IS BAD AND ONLY
+# SERVES TO MAKE TESTING LESS ANNOYING
+# REMOVE THIS OR EVERYTHING IS DOOMED
+secret = "0"*64
+# REMOVE THIS OR EVERYTHING IS DOOMED
+
 secret_c_str = "".join(["\\x" + secret[i:i+2] for i in range(0, len(secret), 2)])
 
 output = f"""\
