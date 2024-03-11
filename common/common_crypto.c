9e83dc88e26c73350f05837f3a21fb91e85ddb5b
Fisher Jepsen <efjepsen@mit.edu>
2024-03-11 11:29:44 -0400

    Add mit_sha256 for easy hashing

--- common/common_crypto.c
+++ common/common_crypto.c
@@ -5,6 +5,17 @@
 
 #include "common_crypto.h"
 
+int mit_sha256(const uint8_t * input, uint8_t len, mit_hash_t * hash) {
+    int ret = wc_Sha256Hash(input, len, hash);
+    if (ret != 0) {
+        printf("error: mit_sha256 failed with errcode %i\n", ret);
+        memset(hash, 0, MIT_HASH_SIZE);
+        return ret;
+    }
+
+    return ret;
+}
+
 // TODO check for null nonces
 
 int mit_encrypt(mit_packet_t * packet, uint8_t * plaintext, uint8_t len) {
--- common/include/common_crypto.h
+++ common/include/common_crypto.h
@@ -10,6 +10,15 @@
 #include "common_msg.h"
 
 #include <wolfssl/wolfcrypt/chacha20_poly1305.h>
+#include <wolfssl/wolfcrypt/sha256.h>
+
+#define MIT_HASH_SIZE SHA256_DIGEST_SIZE
+
+typedef struct __attribute__((packed)) {
+    uint8_t rawBytes[MIT_HASH_SIZE];
+} mit_hash_t;
+
+int mit_sha256(const uint8_t * input, uint8_t len, mit_hash_t * hash);
 
 int mit_encrypt(mit_packet_t * packet, uint8_t * plaintext, uint8_t len);
 int mit_decrypt(mit_packet_t * packet, uint8_t * plaintext);
