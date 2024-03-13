26910bfdb66dd4551061b9854efe8e51b2ce1cfa
Fisher Jepsen <efjepsen@mit.edu>
2024-03-13 16:07:48 -0400

    Increment entire nonce, not just last word

--- common/common_msg.c
+++ common/common_msg.c
@@ -7,7 +7,17 @@
 
 uint8_t null_nonce[MIT_NONCE_SIZE] = {0};
 
-// TODO more than increment just the sequence number
 void increment_nonce(mit_nonce_t * nonce) {
-    nonce->sequenceNumber++;
+    uint32_t * sections = (uint32_t *)nonce->rawBytes;
+    bool carry = true;
+
+    for (int i = sizeof(mit_nonce_t)/sizeof(uint32_t) - 1; i >= 0; i--) {
+        if (carry) {
+            carry = false;
+            if (sections[i] == 0xffffffff) {
+                carry = true;
+            }
+            sections[i]++;
+        }
+    }
 }
